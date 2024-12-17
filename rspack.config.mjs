d71ecc9403cce2ca81bb43f21132c786d39e2c96
Filip Sobol <filipsobol@users.noreply.github.com>
2024-12-17 09:40:10 +0100

    Update rspack config

--- rspack.config.mjs
+++ rspack.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from '@rspack/cli';
 
+// The commented out options are valid, but for some reason increase the bundle size by a lot.
+
 export default defineConfig({
 	entry: './src/index.js',
 	devtool: false,
-	target: 'es2022',
+	// target: 'es2022',
 	output: {
-		chunkFormat: 'module',
+		// chunkFormat: 'module',
+		// chunkLoading: 'import',
 		filename: 'rspack.js',
 		clean: false
 	}
